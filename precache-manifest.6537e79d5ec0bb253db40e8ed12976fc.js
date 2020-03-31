a30cd48b3053c6dab9dad3ed6b97ab3e8df84211
ZweatMonash <zlzwe1@student.monash.edu>
2020-03-31 21:31:05 +1100

    Updates

---
+++ precache-manifest.6537e79d5ec0bb253db40e8ed12976fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cc4a2530d64faf444e9f4eb598dec6",
+    "url": "/webportfolio/index.html"
+  },
+  {
+    "revision": "336fdb412c45ecde5d94",
+    "url": "/webportfolio/static/css/2.5c1d7e3b.chunk.css"
+  },
+  {
+    "revision": "8db4a7dbb43b4440ccf9",
+    "url": "/webportfolio/static/css/main.6bd3ed9f.chunk.css"
+  },
+  {
+    "revision": "336fdb412c45ecde5d94",
+    "url": "/webportfolio/static/js/2.add5c4ad.chunk.js"
+  },
+  {
+    "revision": "b238709882e4cd60e110f01a26ae78cd",
+    "url": "/webportfolio/static/js/2.add5c4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db4a7dbb43b4440ccf9",
+    "url": "/webportfolio/static/js/main.5bf75441.chunk.js"
+  },
+  {
+    "revision": "bb85faa305bebe5194a3",
+    "url": "/webportfolio/static/js/runtime-main.d3ec8bd1.js"
+  },
+  {
+    "revision": "79dee7c7ea3b21b7970003c820f500c1",
+    "url": "/webportfolio/static/media/ObjectSans-Heavy.79dee7c7.otf"
+  },
+  {
+    "revision": "5b82442dbf02699c000fb5d8587c3ca3",
+    "url": "/webportfolio/static/media/ObjectSans-Regular.5b82442d.otf"
+  },
+  {
+    "revision": "bf84b06e08bb6c3d36188a7d6acc03e1",
+    "url": "/webportfolio/static/media/nova.bf84b06e.jpg"
+  }
+]);
